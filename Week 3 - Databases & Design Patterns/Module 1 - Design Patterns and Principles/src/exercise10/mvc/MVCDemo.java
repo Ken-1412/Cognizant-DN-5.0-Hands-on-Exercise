d1f6e5134a98310d5269d412b9f065a62946cb1f
Ketan Singh <ketan@Paradox.local>
2026-07-09 17:35:07 +0530

    feat(week3-m1): implement Exercise 10 - MVC Pattern (Student management application)

---
+++ Week 3 - Databases & Design Patterns/Module 1 - Design Patterns and Principles/src/exercise10/mvc/MVCDemo.java
@@ -0,0 +1,28 @@
+﻿package exercise10.mvc;
+
+public class MVCDemo {
+
+    public static void main(String[] args) {
+
+        Student model = new Student(
+                "Ketan Singh",
+                101,
+                "A"
+        );
+
+        StudentView view = new StudentView();
+
+        StudentController controller =
+                new StudentController(model, view);
+
+        System.out.println("Initial Student Details:");
+        controller.updateView();
+
+        System.out.println("\nAfter Updating Details:");
+
+        controller.setStudentName("Ketan Singh");
+        controller.setStudentGrade("A+");
+
+        controller.updateView();
+    }
+}
